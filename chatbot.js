f492bcf112a5224cb8e785f7ecd4fbc8aa37100c
Le Dang Hoang An <an.le@cyberlogitec.com>
2025-06-18 09:01:17 +0700

    feat: update chatbot

--- chatbot.js
+++ chatbot.js
@@ -8,7 +8,7 @@ import {
   StateGraph,
   MemorySaver,
 } from "@langchain/langgraph";
-import { SystemMessage } from "@langchain/core/messages";
+import { SystemMessage, HumanMessage, AIMessage, trimMessages } from "@langchain/core/messages";
 
 // Define Models
 const llm = new ChatOllama({
@@ -22,9 +22,37 @@ const promptTemplate = ChatPromptTemplate.fromMessages([
   new MessagesPlaceholder("messages")
 ]);
 
+// Load History Chat Messages
+const messages = [
+  new HumanMessage("hi! I'm bob"),
+  new AIMessage("hi!"),
+  new HumanMessage("I like vanilla ice cream"),
+  new AIMessage("nice"),
+  new HumanMessage("whats 2 + 2"),
+  new AIMessage("4"),
+  new HumanMessage("thanks"),
+  new AIMessage("no problem!"),
+  new HumanMessage("having fun?"),
+  new AIMessage("yes!"),
+];
+
+
+// Define Trimmer Message - which limit the number of message that models need to remember
+const trimmer = trimMessages({
+  maxTokens: 11,    // Store maximum 10 messages
+  strategy: "last",    // Store the last messages only based on Max Token.
+  tokenCounter: (msgs) => msgs.length,    // counting the messages
+  includeSystem: false,   //	The SystemMessage is always included if possible.
+  allowPartial: false,   // Partial trimming (e.g. cutting a long message in half) is not allowed
+  startOn: "human",   // Trimming will start from the first Human message and trim pairs from there.
+});
+
+console.log(await trimmer.invoke(messages));
+
 // Define the function that calls the model
 const callModel = async (state) => {
-  const promptValue = await promptTemplate.invoke({messages: state.messages});
+  const trimMessages = await trimmer.invoke(state.messages); // Add new chat in history
+  const promptValue = await promptTemplate.invoke({messages: trimMessages});
   const response = await llm.invoke(promptValue);
   return { messages: response };
 };
@@ -43,21 +71,6 @@ const app = workflow.compile({ checkpointer: memory });
 // Define configuration to distinct each session
 const config = { configurable: { thread_id: uuidv4() } };
 
-// Start the chat
-const input = [
-  {
-    role: "user",
-    content: "Hi! I'm Bob.",
-  },
-];
-
-const input2 = [
-  {
-    role: "user",
-    content: "What is my name?",
-  },
-];
-
-const output = await app.invoke({ messages: input }, config);
-const output2 = await app.invoke({ messages: input2 }, config);
-console.log(output2.messages[output2.messages.length - 1]);
+// Invoke Chat
+const output2 = await app.invoke({ messages: [...messages, new HumanMessage("What is name of mine ?")] }, config);
+console.log(output2.messages[output2.messages.length - 1]["content"]);
